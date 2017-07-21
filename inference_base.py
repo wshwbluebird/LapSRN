22f001c91ce5d56bf2392c7f1c4410983d602e28
151250145 <151250145@smail.nju.edu.cn>
2017-07-21 15:27:15 +0800

    API

--- inference_base.py
+++ inference_base.py
@@ -8,36 +8,36 @@ from PIL import Image
 path1 = "./dataset/BSDS300/images/train/"
 
 
-def change_to_images(paths):
+def change_to_image(path):
     """
     Args:通过路径列表获取 图片
         input_file_path: 输入图像的路径
     Returns:
 
     """
-    im = Image.open(paths[0])
+    im = Image.open(path)
     height = im.size[1]
     width = im.size[0]
-    pic_num = len(paths)
+    pic_num = 1
     num_channel = argument.options.input_channel
     images = []
-    for path in paths:
-        content = tf.read_file(path)
-        image = None
-        if path.endswith("jpg") or path.endswith("jpg"):
-            image =  tf.image.decode_jpeg(content, num_channel)
-        elif path.endswith("png"):
-            image = tf.image.decode_png(content, num_channel)
-        else :
-            print("wrong image: "+path)
 
-        if image is not None:
-            image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-            image = tf.reshape(image,(height,width,3))
-            images.append(image)
+    content = tf.read_file(path)
+    image = None
+    if path.endswith("jpg") or path.endswith("jpg"):
+        image =  tf.image.decode_jpeg(content, num_channel)
+    elif path.endswith("png"):
+        image = tf.image.decode_png(content, num_channel)
+    else :
+        print("wrong image: "+path)
 
-    images = tf.reshape(images,(pic_num,height,width,3))
-    return [images,height,width]
+    if image is not None:
+        image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+        image = tf.reshape(image,(height,width,3))
+
+
+    image = tf.reshape(image,(pic_num,height,width,3))
+    return [image,height,width]
 
 
 def save_image(image , path):
@@ -74,7 +74,7 @@ def is_already_Save(savePath):
 """
 
 
-def predict_batch(input_image,paths,scale, ori_height, ori_width):
+def predict_single(input_image,path,scale, out_height, out_width):
     """
         tensorflow op, process the input files in input_images
         return predicted 2x, 4x, 8x images
@@ -82,44 +82,37 @@ def predict_batch(input_image,paths,scale, ori_height, ori_width):
     argument.options.predict(1)
     factor = get_scale_factor(scale)
     hr2_predict, hr4_predict, hr8_predict = net.get_LasSRN(input_image)
-    hr_height = int(ori_height * scale)
-    hr_width = int(ori_width * scale)
-
 
     """
         determine the floor picture
     """
     if factor == 1:
-        images = input_image
+        image = input_image
     elif factor == 2:
-        images = hr2_predict
+        image = hr2_predict
     elif factor == 4:
-        images = hr4_predict
+        image = hr4_predict
     else:
-        images = hr8_predict
+        image = hr8_predict
 
 
     """
         resize to the real
     """
-    images = [tf.image.resize_images(images[i], [hr_height, hr_width], method=tf.image.ResizeMethod.BICUBIC)
-              for i in range(len(paths))]
 
-    images = [ tf.image.convert_image_dtype(images[i], dtype=tf.uint8) for i in range(len(paths))]
-    back_images = []
-    for i in range(len(paths)):
-        path = paths[i]
-        image = images[i]
-        if path.endswith("jpg") or path.endswith("jpg"):
-            image = tf.image.encode_jpeg(image)
-        elif path.endswith("png"):
-            image = tf.image.encode_png(image)
-        else:
-            print("wrong image: " + path)
-            return
-        back_images.append(image)
+    image = tf.image.resize_images(image[0], [out_height, out_width], method=tf.image.ResizeMethod.BICUBIC)
+    image = tf.image.convert_image_dtype(image, dtype=tf.uint8)
+    print(image)
 
-    return back_images
+    if path.endswith("jpg") or path.endswith("jpg"):
+        image = tf.image.encode_jpeg(image)
+    elif path.endswith("png"):
+        image = tf.image.encode_png(image)
+    else:
+        print("wrong image: " + path)
+        return
+
+    return image
 
 
 """
@@ -127,7 +120,7 @@ def predict_batch(input_image,paths,scale, ori_height, ori_width):
 """
 
 
-def batch_inference(input_file_paths, output_dir_paths, scale):
+def single_inference(input_file_path, output_dir_path, scale, out_height, out_width):
     """
     Args:
         input_file_paths:  输入的路径paths
@@ -142,8 +135,8 @@ def batch_inference(input_file_paths, output_dir_paths, scale):
         print("no model please train a model first")
         return
 
-    input_images,height,width = change_to_images(input_file_paths)
-    hr_predict = predict_batch(input_images, input_file_paths, scale, height, width)
+    input_images,height,width = change_to_image(input_file_path)
+    hr_predict = predict_single(input_images, input_file_path, scale, out_height, out_width)
 
     saver = tf.train.Saver()
     result = []
@@ -151,22 +144,10 @@ def batch_inference(input_file_paths, output_dir_paths, scale):
     with tf.Session() as sess:
         saver.restore(sess, save_path)
         with tf.device('/cpu:0'):
-            for i in range(len(input_file_paths)):
-                hr_img = sess.run(hr_predict)
-                save_image(hr_img[i],output_dir_paths[i])
-                dict={'input':input_file_paths[i],'out_put':output_dir_paths[i]}
-                result.append(dict)
-    return result
+            hr_img = sess.run(hr_predict)
+            save_image(hr_img,output_dir_path)
+            return output_dir_path
 
-def single_inference(input_file_path, output_dir_path, scale):
-    """
-    Args:
-        input_file_path:  输入的路径path
-        output_dir_path:  输出的路径path
-        scale:
 
-    Returns:
 
-    """
-    return  batch_inference([input_file_path],[output_dir_path],scale)[0]
 
