bddd534a6b29dc9f9396bb4612daa28bb2648b66
151250145 <151250145@smail.nju.edu.cn>
2017-07-21 10:15:54 +0800

    预测单一图片

--- inference2.py
+++ inference2.py
@@ -3,10 +3,11 @@ import tensorflow as tf
 import argument
 import net
 import os
+from PIL import Image
 import scipy.misc
 
 path1 = "./dataset/BSDS300/images/train/"
-def change_to_images(input_file_paths):
+def change_to_image(path):
     """
     Args:通过路径列表获取 图片
         input_file_paths:
@@ -14,34 +15,31 @@ def change_to_images(input_file_paths):
     Returns:
 
     """
-    patch = []
+    im = Image.open(path)
+    height = im.size[0]
+    weight = im.size[1]
     num_channel = argument.options.input_channel
-    for path in input_file_paths:
-        content = tf.read_file(path)
-        image = None
-        if path.endswith("jpg") or path.endswith("jpg"):
-            image =  tf.image.decode_jpeg(content, num_channel)
-        elif path.endswith("png"):
-            image = tf.image.decode_png(content, num_channel)
-        else :
-            print("wrong image: "+path)
+    content = tf.read_file(path)
+    image = None
+    if path.endswith("jpg") or path.endswith("jpg"):
+        image =  tf.image.decode_jpeg(content, num_channel)
+    elif path.endswith("png"):
+        image = tf.image.decode_png(content, num_channel)
+    else :
+        print("wrong image: "+path)
 
-        if image is not None:
-            image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-            patch.append(image)
-        return patch
+    if image is not None:
+        image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+        print(image)
+        image = tf.reshape(image,(1,height,weight,3))
+        print(image)
+        return image
+
+
+
 
 
 def save_image(image , path):
-    image = tf.image.convert_image_dtype(image, dtype=tf.uint8)
-    if path.endswith("jpg") or path.endswith("jpg"):
-        image = tf.image.encode_jpeg(image)
-    elif path.endswith("png"):
-        image = tf.image.encode_png(image)
-    else:
-        print("wrong image: " + path)
-        return
-
     with open(path, "wb") as file:
         file.write(image)
 
@@ -56,15 +54,32 @@ def is_already_Save(savePath):
     print(os.getcwd())
     return os.path.exists(savePath + ".meta")
 
-
-def predict(input_images):
+def predict_single(input_image,path,scale):
     """
-        tensorflow op, process all the input files in data_paths(input_images)
+        tensorflow op, process all the input files in data_paths(input_image)
         return predicted 2x, 4x, 8x images
     """
-    argument.options.predict(len(input_images))
-    hr2_predict, hr4_predict, hr8_predict = net.get_LasSRN(input_images)
-    return [hr2_predict, hr4_predict, hr8_predict]
+    argument.options.predict(1)
+    hr2_predict, hr4_predict, hr8_predict = net.get_LasSRN(input_image)
+    image = None
+    if scale == 2:
+        image = hr2_predict
+    elif scale == 4:
+        image = hr4_predict
+    else:
+        image = hr8_predict
+
+    image = tf.image.convert_image_dtype(image[0], dtype=tf.uint8)
+    if path.endswith("jpg") or path.endswith("jpg"):
+        image = tf.image.encode_jpeg(image)
+    elif path.endswith("png"):
+        image = tf.image.encode_png(image)
+    else:
+        print("wrong image: " + path)
+        return
+
+    return image
+
 
 
 """
@@ -72,7 +87,9 @@ def predict(input_images):
 """
 
 
-def batch_inference(input_file_paths, output_dir_paths, scale_list):
+
+
+def single_inference(input_file_path, output_dir_path, scale):
     """
 
     Args:
@@ -88,25 +105,18 @@ def batch_inference(input_file_paths, output_dir_paths, scale_list):
         print("no model please train a model first")
         return
 
-    input_images = change_to_images(input_file_paths)
-    hr2_predict, hr4_predict, hr8_predict = predict(input_images)
+    input_image = change_to_image(input_file_path)
+    print(input_image)
+    hr_predict= predict_single(input_image,input_file_path,scale)
+
     saver = tf.train.Saver()
 
     with tf.Session() as sess:
         saver.restore(sess, save_path)
         with tf.device('/cpu:0'):
-            hr2, hr4, hr8 = sess.run([hr2_predict, hr4_predict, hr8_predict])
-            hr_img= None
-            for i in range(len(input_file_paths)):
-                if scale_list[i] == 2:
-                    hr_img = hr2[i]
-                elif scale_list[i] == 4:
-                    hr_img = hr4[i]
-                else:
-                    hr_img = hr8[i]
-
-            save_image(hr_img,output_dir_paths[i])
-
+            hr_img = sess.run(hr_predict)
+            save_image(hr_img,output_dir_path)
+            print('predict successfully in '+output_dir_path)
 
 if __name__ == '__main__':
-    batch_inference(path1+'2092.jpg','./hr.jpg',[2])
+    single_inference(path1+'2092.jpg','./hr.jpg',2)
